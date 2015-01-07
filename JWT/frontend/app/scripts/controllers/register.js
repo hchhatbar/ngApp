38d22d86d2e4761c4bf03ca58e6616dbee371a8a
Hemen Chhatbar <hemenc@slalom.com>
2015-01-06 23:09:19 -0800

    express install

--- JWT/frontend/app/scripts/controllers/register.js
+++ JWT/frontend/app/scripts/controllers/register.js
@@ -5,8 +5,10 @@ angular.module('jwtApp')
   .controller('RegisterCtrl', function ($scope,$rootScope,$http,alert) {
     $scope.submit = function(){
     	console.log('submit');
-    	var url = '/';
-    	var user = {};
+    	var url = 'http://localhost:3000/register';
+    	var user = {
+            name: 'Hemen'
+        };
     	$http.post(url,user)
     	.success(function(err) {
     		alert('success', 'Ok!', 'You are now registered.');
